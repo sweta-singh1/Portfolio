822a713e7c043875bdb48f12e76b999f7d7f3c85
Sweta Singh <swetasingh4661@gmail.com>
2025-05-01 23:51:07 +0530

    second coomit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero/Hero";
 import About from "./About/About";
-import Services from "./Services/services";
+// import Services from "./Services/services";
 // import MyWork from "./MyWork/mywork";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Footer/Footer";
 import MyWork from "./MyWork/MyWork";
+import Services from "./Services/services";
 
 const App = () => {
   return (
@@ -15,7 +16,7 @@ const App = () => {
       <Hero />
       <About />
       <Services />
-     
+
       <MyWork />
       <Contact />
       <Footer />
